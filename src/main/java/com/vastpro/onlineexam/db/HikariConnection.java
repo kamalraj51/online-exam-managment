04ebd157db61a1524be1cdf517dcb50c7b0420aa
Vastpro-12 <Vastpro-12@desktop>
2026-01-29 23:03:32 +0530

    log hikar by kamal

---
+++ src/main/java/com/vastpro/onlineexam/db/HikariConnection.java
@@ -0,0 +1,34 @@
+package com.vastpro.onlineexam.db;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+public class HikariConnection {
+
+			private static HikariDataSource dataSource;
+		
+			static {
+				HikariConfig config=new HikariConfig();
+				config.setJdbcUrl("jdbc:postgresql://localhost:5432/onlineexam_db");
+				config.setUsername("postgres");
+				config.setPassword("postgres");
+				
+				config.setMinimumIdle(5);
+				config.setMaximumPoolSize(10);
+				config.setConnectionTimeout(20000);
+				config.setIdleTimeout(600000);
+				config.setMaxLifetime(18000);
+				config.setDataSourceJNDI("jdbc/onlineexam_db");
+				
+				config.setPoolName("hikari_pool");
+				dataSource=new HikariDataSource(config);
+				
+				
+			}
+			
+	
+			public static DataSource getDataSource() {
+				return dataSource;
+			}
+	}
